8bce5a2f039b8aa2306180b8b981beba49d1f2fc
Tushar Banik <b.toshar@iitg.ac.in>
2024-02-04 13:46:39 +0530

    middleware modification

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,26 @@
 import { authMiddleware } from "@clerk/nextjs";
 import { NextRequest, NextResponse } from "next/server";
- 
+
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ["/login"],
-  afterAuth(auth, req:NextRequest){
-        if (auth.userId && req.nextUrl.pathname === '/login'){
-          const url = req.nextUrl.clone()
-          url.pathname = '/'
-          return NextResponse.redirect(url)
-        }
-  }
+  afterAuth(auth, req: NextRequest) {
+    if (!auth.userId && req.nextUrl.pathname !== "/login") {
+      const url = req.nextUrl.clone();
+      url.pathname = "/login";
+      return NextResponse.redirect(url);
+    }
+    
+    if (auth.userId && req.nextUrl.pathname === "/login") {
+      const url = req.nextUrl.clone();
+      url.pathname = "/";
+      return NextResponse.redirect(url);
+    }
+  },
 });
- 
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
