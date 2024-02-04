8e2c3de5c1f999d42c71eaf03d21f5f766f9f667
Tushar Banik <b.toshar@iitg.ac.in>
2024-02-04 12:39:40 +0530

    authentication using clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-   <div>
-    pdf to chat webapp
-   </div>
+    <div className="h-screen w-screen flex items-center justify-center bg-black">
+      <Button asChild variant="secondary">
+        {/* <Link href="/login">Login</Link> */}
+        <UserButton afterSignOutUrl="/"/>
+      </Button>
+    </div>
   );
 }
