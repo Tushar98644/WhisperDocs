59bfe4ce95bd31fc5230eaa84577ec7d1e3c8c13
Tushar Banik <b.toshar@iitg.ac.in>
2024-02-05 12:59:45 +0530

    login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,37 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+"use client";
+import { GoogleGeminiEffect } from "@/components/ui/google-gemini-effect";
+import { useScroll, useTransform } from "framer-motion";
+import React from "react";
 
-const Login = () => {
-    return ( 
-        <div>
-               <Button asChild variant="secondary">
-                    <Link href="/sign-in">Login</Link>
-                </Button>
-        </div>
-     );
+const Login =()=> {
+  const ref = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const pathLengthFirst = useTransform(scrollYProgress, [0, 0.8], [0.2, 1.2]);
+  const pathLengthSecond = useTransform(scrollYProgress, [0, 0.8], [0.15, 1.2]);
+  const pathLengthThird = useTransform(scrollYProgress, [0, 0.8], [0.1, 1.2]);
+  const pathLengthFourth = useTransform(scrollYProgress, [0, 0.8], [0.05, 1.2]);
+  const pathLengthFifth = useTransform(scrollYProgress, [0, 0.8], [0, 1.2]);
+
+  return (
+    <div
+      className="h-[400vh] bg-black w-full dark:border dark:border-white/[0.1] relative pt-40 overflow-clip"
+      ref={ref}
+    >
+      <GoogleGeminiEffect
+        pathLengths={[
+          pathLengthFirst,
+          pathLengthSecond,
+          pathLengthThird,
+          pathLengthFourth,
+          pathLengthFifth,
+        ]}
+      />
+    </div>
+  );
 }
- 
+
 export default Login;
