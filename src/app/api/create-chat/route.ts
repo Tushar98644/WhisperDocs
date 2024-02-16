4ab59954c0afcc0e6309f2c578c9ed575b6786b1
Tushar Banik <b.toshar@iitg.ac.in>
2024-02-16 21:15:22 +0530

    react-query provider

---
+++ src/app/api/create-chat/route.ts
@@ -0,0 +1,3 @@
+export const GET = async(request : Request, response: Response)=>{
+     
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
+import QueryProvider from "@/providers/query_provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
+      <QueryProvider>
         <html lang="en">
           <body className={inter.className}>{children}</body>
         </html>
+      </QueryProvider>
     </ClerkProvider>
   );
 }
