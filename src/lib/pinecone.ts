0eb59ccb08475f5426c9283742e19bed15262edf
Tushar Banik <b.toshar@iitg.ac.in>
2024-02-17 18:00:38 +0530

    pinecone config

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,82 +1,97 @@
-import { Pinecone } from '@pinecone-database/pinecone'
-import { getObjectfroms3 } from './s3-pincecone';
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { Pinecone, PineconeRecord } from "@pinecone-database/pinecone";
+import { getObjectfroms3 } from "./s3-pincecone";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import {
-    Document,
-    RecursiveCharacterTextSplitter,
-  } from "@pinecone-database/doc-splitter";
+  Document,
+  RecursiveCharacterTextSplitter,
+} from "@pinecone-database/doc-splitter";
+import md5 from "md5";
+import { getEmbeddings } from "./embeddings";
+import { convertToAscii } from "./utils";
 
 interface PdfPage {
-    pageContent: string;
-    metadata: {
-        loc: {
-            pageNumber: number;
-        }
-    }
+  pageContent: string;
+  metadata: {
+    loc: {
+      pageNumber: number;
+    };
+  };
 }
-let PinceconeClient : Pinecone ;
+let PinceconeClient: Pinecone;
 
 export const getPinceconeClient = () => {
-    if (!PinceconeClient) {
-        PinceconeClient = new Pinecone({
-            apiKey: process.env.PINECONE_API_KEY as string,
-        });
-    }
-    return PinceconeClient;
-}
+  if (!PinceconeClient) {
+    PinceconeClient = new Pinecone({
+      apiKey: process.env.PINECONE_API_KEY as string,
+    });
+  }
+  return PinceconeClient;
+};
 
 export const uploads3toPinecone = async (file_key: string) => {
-    try{
-        const file_path = await getObjectfroms3(file_key);
-        if (!file_path) {
-            throw new Error("File not found");
-        }
-        const loader = new PDFLoader(file_path);
-        const pages = await loader.load();
-        return pages;
-    } 
-    catch(e){
-        console.log(`There was an error uploading the file to Pinecone: ${e}`);
+  try {
+    const file_path = await getObjectfroms3(file_key);
+    if (!file_path) {
+      throw new Error("File not found");
     }
-}
+    const loader = new PDFLoader(file_path);
+    const pages = (await loader.load()) as PdfPage[];
 
-const embedDocument =async(doc: Document) => {
-    try {
-      const embeddings = await getEmbeddings(doc.pageContent);
-      const hash = md5(doc.pageContent);
-  
-      return {
-        id: hash,
-        values: embeddings,
-        metadata: {
-          text: doc.metadata.text,
-          pageNumber: doc.metadata.pageNumber,
-        },
-      } as PineconeRecord;
-    } catch (error) {
-      console.log("error embedding document", error);
-      throw error;
-    }
+    // 2. split and segment the pdf
+  const documents = await Promise.all(pages.map(prepareDocument));
+
+  // 3. vectorise and embed individual documents
+  const vectors = await Promise.all(documents.flat().map(embedDocument));
+  const client = getPinceconeClient();
+  const pineconeIndex = client.index('whisper-docs');
+  const namespace = pineconeIndex.namespace(convertToAscii(file_key));
+
+  console.log("inserting vectors into pinecone");
+  await namespace.upsert(vectors);
+
+  return documents[0];
+  } catch (e) {
+    console.log(`There was an error uploading the file to Pinecone: ${e}`);
   }
-  
-   const truncateStringByBytes = (str: string, bytes: number) => {
-    const enc = new TextEncoder();
-    return new TextDecoder("utf-8").decode(enc.encode(str).slice(0, bytes));
-  };
-  
-  const  prepareDocument =async(page: PdfPage) => {
-    let { pageContent, metadata } = page;
-    pageContent = pageContent.replace(/\n/g, "");
-    // split the docs
-    const splitter = new RecursiveCharacterTextSplitter();
-    const docs = await splitter.splitDocuments([
-      new Document({
-        pageContent,
-        metadata: {
-          pageNumber: metadata.loc.pageNumber,
-          text: truncateStringByBytes(pageContent, 36000),
-        },
-      }),
-    ]);
-    return docs;
+};
+
+const embedDocument = async (doc: Document) => {
+  try {
+    const embeddings = await getEmbeddings(doc.pageContent);
+    const hash = md5(doc.pageContent);
+
+    return {
+      id: hash,
+      values: embeddings,
+      metadata: {
+        text: doc.metadata.text,
+        pageNumber: doc.metadata.pageNumber,
+      },
+    } as PineconeRecord;
+  } catch (error) {
+    console.log("error embedding document", error);
+    throw error;
   }
+};
+
+const truncateStringByBytes = (str: string, bytes: number) => {
+  const enc = new TextEncoder();
+  return new TextDecoder("utf-8").decode(enc.encode(str).slice(0, bytes));
+};
+
+const prepareDocument = async (page: PdfPage) => {
+  let { pageContent, metadata } = page;
+  pageContent = pageContent.replace(/\n/g, "");
+  // split the docs
+  const splitter = new RecursiveCharacterTextSplitter();
+  const docs = await splitter.splitDocuments([
+    new Document({
+      pageContent,
+      metadata: {
+        pageNumber: metadata.loc.pageNumber,
+        text: truncateStringByBytes(pageContent, 36000),
+      },
+    }),
+  ]);
+  return docs;
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const convertToAscii = (inputString:string) => {
+  const asciiString = inputString.replace(/[^\x00-\x7F]+/g, "");
+  return asciiString;
+}
