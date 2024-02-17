39c09c6b499009a297ae486c6045e797bada1de3
Tushar Banik <b.toshar@iitg.ac.in>
2024-02-17 17:32:33 +0530

    dependabot config

--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,7 +1,19 @@
 import { Pinecone } from '@pinecone-database/pinecone'
 import { getObjectfroms3 } from './s3-pincecone';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import {
+    Document,
+    RecursiveCharacterTextSplitter,
+  } from "@pinecone-database/doc-splitter";
 
+interface PdfPage {
+    pageContent: string;
+    metadata: {
+        loc: {
+            pageNumber: number;
+        }
+    }
+}
 let PinceconeClient : Pinecone ;
 
 export const getPinceconeClient = () => {
@@ -27,3 +39,44 @@ export const uploads3toPinecone = async (file_key: string) => {
         console.log(`There was an error uploading the file to Pinecone: ${e}`);
     }
 }
+
+const embedDocument =async(doc: Document) => {
+    try {
+      const embeddings = await getEmbeddings(doc.pageContent);
+      const hash = md5(doc.pageContent);
+  
+      return {
+        id: hash,
+        values: embeddings,
+        metadata: {
+          text: doc.metadata.text,
+          pageNumber: doc.metadata.pageNumber,
+        },
+      } as PineconeRecord;
+    } catch (error) {
+      console.log("error embedding document", error);
+      throw error;
+    }
+  }
+  
+   const truncateStringByBytes = (str: string, bytes: number) => {
+    const enc = new TextEncoder();
+    return new TextDecoder("utf-8").decode(enc.encode(str).slice(0, bytes));
+  };
+  
+  const  prepareDocument =async(page: PdfPage) => {
+    let { pageContent, metadata } = page;
+    pageContent = pageContent.replace(/\n/g, "");
+    // split the docs
+    const splitter = new RecursiveCharacterTextSplitter();
+    const docs = await splitter.splitDocuments([
+      new Document({
+        pageContent,
+        metadata: {
+          pageNumber: metadata.loc.pageNumber,
+          text: truncateStringByBytes(pageContent, 36000),
+        },
+      }),
+    ]);
+    return docs;
+  }
